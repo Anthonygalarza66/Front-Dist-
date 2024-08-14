06f1f13b375c44bc4cb8a13007297897b7b3238a
Freylolo <freylolo20@outlook.com>
2024-08-13 23:03:20 -0500

    Dist AD Seg

---
+++ dist/sistema-ad-seg/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-2ZUZHFJQ.mjs";import"./chunk-DDYTUN32.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
